6369fedb10c7ce4521b3b1ab0310fc3046b1ba54
Nicholas <nik.kyriakides@gmail.com>
2024-11-03 22:43:51 +0200

    refactor: rubocopiing

--- blog/nix.rb
+++ blog/nix.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'bundler/inline'
 
@@ -13,83 +14,96 @@ gemfile do
 end
 
 class Site
-  attr_reader :layouts, :pages
+  attr_reader :layouts, :variables
 
-  def initialize layouts = []
-    @layouts = layouts.map do |layout| [layout.name, layout] end.to_h 
+  def initialize(layouts = [])
+    @variables = nil
+    @layouts = layouts.to_h { |layout| [layout.name, layout] }
     @docs = []
   end
-  
-  def add pages
-    @docs = @docs + pages
+
+  def add(pages)
+    @docs += pages
     self
   end
-  
-  def pages 
-    @docs.filter do |doc| doc.is_a?(HTMLPage) end  
+
+  def pages
+    @docs.filter { |doc| doc.is_a?(HTMLPage) }
   end
-  
-  def compile variables:
+
+  def compile(variables:)
+    @variables = variables
     @docs
-      .map do |page| page.compile layouts:, ctx: { pages:, variables: } end
-      .flatten.uniq do |page| page.path end
+      .map(&compile_page)
+      .flatten
+      .uniq(&:path)
       .map(&:to_entry)
+  end
+
+  def compile_page
+    lambda do |page|
+      page.compile layouts:, ctx: { pages:, variables: }
+    end
   end
 end
 
 class Layout
   attr_reader :name
 
-  def initialize path, html
+  def initialize(path, html)
     @name = path.basename(path.extname).to_s
     @html = html
   end
-  
-  def to_s() @html end
+
+  def to_s = @html
 end
 
 class Document
   attr_reader :path, :data
 
-  def initialize path, data = nil
+  def initialize(path, data = nil)
     @path = Pathname.new path.to_s
     @name = @path.parent.basename.to_s
     @data = data
   end
-  
-  def to_entry() [@path, @data] end
+
+  def to_entry = [@path, @data]
 end
 
 class HTMLPage < Document
-  def initialize path:, title:
-    super path
-    @title = title ||= @path.basename.to_s
+  def initialize(path:, title:)
+    super(path)
+    @title = title || @path.basename.to_s
   end
 
-  def compile layouts:, ctx:   
+  def compile(layouts:, ctx:)
     type  = self.class.to_s.downcase
     name  = @path.basename(@path.extname).to_s
 
-    @data = replace (
-       <<~BODY
-        #{layouts['header']}
-          <main class="#{type} #{name}">#{render(ctx)}</main>
-         #{layouts['footer']}
-        BODY
-    ), ctx[:variables]
+    html = <<~BODY
+      #{layouts['header']}
+        <main class="#{type} #{name}">#{render(ctx)}</main>
+       #{layouts['footer']}
+    BODY
+
+    @data = replace(html, ctx[:variables])
 
     self
-  end 
-  
-  def render(*) '' end
-  def to_s() @data end
+  end
 
-  private def replace html, variables
-    { **variables, 
-      'title' => @title, 
-      'bytes' => html.gsub(/\s+/, '').bytesize.to_s 
-    }.reduce html do | html, (variable, value) | 
-      html = html.gsub('{{' + variable.to_s + '}}', value.to_s) 
+  def render(*) = ''
+  def to_s = @data
+
+  private
+
+  def replace(html, variables)
+    {
+      **variables,
+      'title' => @title,
+      'bytes' => html.gsub(/\s+/, '').bytesize.to_s
+    }
+      .reduce(html) do |replaced, (variable, value)|
+      replaced.gsub("{{#{variable}}}", value.to_s)
     end
   end
 end
@@ -99,63 +113,74 @@ end
 class MarkdownPage < HTMLPage
   attr_reader :title
 
-  def initialize path, markdown, title = nil
-    super(path:, title: title ? title : to_title(markdown))
+  def initialize(path, markdown, title = nil)
+    super(path:, title: title || to_title(markdown))
     @markdown = markdown
   end
-  
-  def render ctx
-    super(ctx) + Kramdown::Document.new(@markdown, {
-      input: 'GFM', auto_ids: true,
-      syntax_highlighter: 'rouge'
-    }).to_html
+
+  def render(ctx)
+    super +
+      Kramdown::Document
+      .new(@markdown, { input: 'GFM', syntax_highlighter: 'rouge' })
+      .to_html
   end
-  
-  private def to_title markdown
-    markdown.lines.first&.start_with?('# ') ? 
-      markdown.lines.first[2..-1].strip : 'Untitled'
+
+  private
+
+  def to_title(markdown)
+    if markdown.lines.first&.start_with?('# ')
+      markdown.lines.first[2..].strip
+    else
+      'Untitled'
+    end
   end
 end
 
-class Page < MarkdownPage 
-  def initialize path, markdown
-    super "/#{path.basename(path.extname)}/index.html", markdown
+class Page < MarkdownPage
+  def initialize(path, markdown)
+    super("/#{path.basename(path.extname)}/index.html", markdown)
   end
 end
 
 class Post < MarkdownPage
   attr_reader :date, :link
-  
-  def initialize path, markdown
-    @date = Date.parse(markdown.lines[2]&.strip || '') rescue Date.today
+
+  def initialize(path, markdown)
+    @date = begin
+      Date.parse(markdown.lines[2]&.strip || '')
+    rescue StandardError
+      Date.today
+    end
     @link = "/posts/#{path.basename(path.extname)}"
-    super "/posts/#{path.basename(path.extname)}/index.html", markdown
+    super("/posts/#{path.basename(path.extname)}/index.html", markdown)
   end
-  
-  def render ctx
-    super(ctx) + '<link rel="stylesheet" href="/public/highlight.css"><link>'
+
+  def render(ctx)
+    "#{super}<link rel=\"stylesheet\" href=\"/public/highlight.css\"><link>"
   end
 end
 
-class Index < MarkdownPage 
-  def initialize path, markdown
-    super path + '/index.html', markdown, 'Home'
+class Index < MarkdownPage
+  def initialize(path, markdown)
+    super("#{path}/index.html", markdown, 'Home')
   end
 
-  def render ctx
-    ispost = -> item do item.class.to_s == 'Post' end
-    list_items = ctx[:pages].filter(&ispost).reduce('') do |list,post| 
-      list << post = <<~BODY
-      <li>
-        <a href="#{post.link}">
-          <h3>#{post.title}</h3> 
-          <small>#{post.date.strftime('%b, %Y')}</small> 
-        </a>
-      </li>
+  def render(ctx)
+    ispost = lambda do |item|
+      item.instance_of?(::Post)
+    end
+    list_items = ctx[:pages].filter(&ispost).reduce(+'') do |list, post|
+      list << <<~BODY
+        <li>
+          <a href="#{post.link}">
+            <h3>#{post.title}</h3>#{' '}
+            <small>#{post.date.strftime('%b, %Y')}</small>#{' '}
+          </a>
+        </li>
       BODY
     end
 
-    super(ctx) + <<~BODY.squeeze("\n")
+    super + <<~BODY.squeeze("\n")
       <ul class="list">
         #{list_items}
       </ul>
@@ -165,104 +190,105 @@ end
 
 # -- Initialisation --
 
-$0 = "nix"
+$PROGRAM_NAME = 'nix'
 NO_COLORS = ENV['NO_COLORS'] || !$stdout.tty?
 
 class NoConfigError < StandardError
   attr_reader :detail
 
   def initialize
-    @detail = <<~BODY      
+    super
+
+    @detail = <<~BODY
       #{color('ERROR')}
       Cannot find _config.yml in current directory
       #{color('RESET')}#{color('WARN')}
       If you didnt create a site yet, run:\n
-      $ nix --init 
+      $ nix --init#{' '}
       #{color('RESET')}
-     BODY
+    BODY
   end
 end
 
-def color level
-  color = { 'WARN' => 33, 'INFO' => 32, 'ERROR' => 31, 'FATAL' => 31 }[level]
-  NO_COLORS ? '' : "\e[0;#{color}m"
+def color(lvl = 'reset')
+  color = { 'WARN' => 33, 'INFO' => 32, 'ERROR' => 31, 'FATAL' => 31 }[lvl]
+  if ENV['NO_COLORS']
+    ''
+  else
+    lvl.downcase == 'reset' ? "\033[m" : "\e[0;#{color}m"
+  end
 end
 
 Log = Logger.new $stdout
-Log.formatter = proc do |level, datetime, progname, msg|  
-  if level == 'FATAL' 
+Log.formatter = proc do |lvl, _datetime, _progname, msg|
+  if lvl == 'FATAL'
     puts msg&.detail ||= ''
     raise msg
   else
-    puts "#{color(level)}#{level} #{msg}#{color(level) ? color('reset') : '' }"
+    puts "#{color(lvl)}#{lvl} #{msg}#{color(lvl) ? color('reset') : ''}"
   end
 end
 
 module FS
-  def self.create(type) -> path do type.new(path, File.read(path)) end end
+  def self.create(type) = lambda do |path|
+    type.new(path, File.read(path))
+  end
 
-  def self.write(dest:, force: false) 
-    -> ((path, data)) do 
+  def self.write(dest:, force: false)
+    lambda do |(path, data)|
       pathname = Pathname.new File.join(dest, path)
-      
+
       FileUtils.mkdir_p pathname.dirname
-      
-      if File.exist?(pathname) && !force
-        return Log.warn("| write |#{pathname} | skipped, exists") 
-      end
-  
-      File.write(pathname, data); 
-      Log.debug "| write | #{pathname}"
+
+      Log.warn("write #{pathname}, skipped. exists") if
+        File.exist?(pathname) && !force
+
+      File.write(pathname, data)
+      Log.debug "write #{pathname} ok"
     end
   end
 end
 
-def init url 
-  Log.info "| fetching | samples from: #{url} ..."
-  JSON.load(URI.open(url)).each(&FS::write(dest: './')) 
-end 
+def init(url)
+  Log.info "fetching files from: #{url} ..."
+  JSON.parse(URI.parse(url).open.read)['files'].each(&FS.write(dest: './'))
+end
 
-def build config 
+def build(config)
   base = config['base']
-  dest = config['dest']   
+  dest = config['dest']
 
-Site
-  .new(Pathname.glob('_layouts/*.html', base:).map(&FS::create(Layout)))
-  .add(Pathname.glob('posts/*[^index]*.md', base:).map(&FS::create(Post)))
-  .add(Pathname.glob('pages/*[^index].md', base:).map(&FS::create(Page)))
-  .add(Pathname.glob('pages/index.md', base:).map(&FS::create(Index)))
-  .compile(variables: config) 
-  .map(&FS::write(dest:, force: true))
+  Site
+    .new(Pathname.glob('_layouts/*.html', base:).map(&FS.create(Layout)))
+    .add(Pathname.glob('posts/*[^index]*.md', base:).map(&FS.create(Post)))
+    .add(Pathname.glob('pages/*[^index].md', base:).map(&FS.create(Page)))
+    .add(Pathname.glob('pages/index.md', base:).map(&FS.create(Index)))
+    .compile(variables: config)
+    .map(&FS.write(dest:, force: true))
 
-  FileUtils.cp_r(File.join(base, 'public/'), File.join(dest, 'public'))  
+  FileUtils.cp_r(File.join(base, 'public/'), File.join(dest, 'public'))
 end
 
 params = {}
-opts = OptionParser.new("\ndocs: https://github.com/nicholaswmin/nix \n") do |o|
-  o.on('-i', '--init',  "create new sample site")
-  o.on('-b', '--build', "build site to output")
-  o.on('-s PORT', '--serve PORT', "start dev. server at PORT", Integer)
-  o.on('-h', '--help',  "print this help")
-end; opts.parse!(into: params)
-
-if params[:help] || params.empty?
-  puts $0, color('INFO'), opts.help, color('reset')
+opts = OptionParser.new('docs: https://github.com/nicholaswmin/nix') do |o|
+  o.on('-i', '--init',  'create new sample site')
+  o.on('-b', '--build', 'build site to output')
+  o.on('-s', '--serve', 'serve site at port', Integer)
+  o.on('-h', '--help',  'print this help')
 end
+opts.parse!(into: params)
 
 if params[:build]
-  build YAML.load_file '_config.yml' rescue
-    Log.fatal NoConfigError.new
+  build YAML.load_file '_config.yml'
 
-  Log.info '| build | ok'
+  Log.info 'build ok'
 end
 
-if (params[:init])
+if params[:init]
   init 'https://raw.githubusercontent.com/nicholaswmin/nix/main/init.json'
-  Log.info '| init | ok | you should commit & push these files!'
+  Log.info 'init ok, you should commit & push these files!'
 end
 
-if params[:serve]
-  spawn "ruby -run -e httpd . -p #{params[:serve] ||= 0}"
-end
+spawn "ruby -run -e httpd . -p #{params[:serve] ||= 0}" if params[:serve]
 
 puts "\n"
---
+++ blog/posts/terms.md
@@ -0,0 +1,13 @@
+# just another post
+
+21-11-2023
+
+This is another sample post, written in **Markdown**.
+
+You can add as many as you want in this folder but ensure each post:
+
+- has an `h1` heading at the very top
+- has an empty line following it
+- and a date on the 3rd line
+
+... just like this post.
