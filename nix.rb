0bf2df0dce79c33ac198c3e12ca6bbddc0cd4ec6
Nicholas <nik.kyriakides@gmail.com>
2024-11-06 16:15:05 +0200

    again

--- nix.rb
+++ nix.rb
@@ -68,11 +68,11 @@ class HTMLPage < Document
         <main class="#{type} #{name}">#{render(ctx)}</main>
       #{layouts['footer']}
     BODY
-    @data = replace(html, ctx[:variables].merge({ base_url: base_url }))
+    @data = replace(html, ctx[:variables].merge({ assets_url: assets_url }))
     self
   end
   
-  def base_url(basename = '')
+  def assets_url(basename = '')
     File.join(path.each_filename.to_a.drop(1).map { '../' }
       .join(''), basename).delete_prefix('/')
   end
@@ -116,7 +116,7 @@ class Post < MarkdownPage
   end
 
   def render(ctx)
-    "#{super}<link rel=\"stylesheet\" href=\"#{base_url('highlight.css')}\">
+    "#{super}<link rel=\"stylesheet\" href=\"#{assets_url('highlight.css')}\">
     <link>"
   end
 end
