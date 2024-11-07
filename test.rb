8210503b28d35b2405bb1f4d9347ab5c0fa168af
Nicholas <nik.kyriakides@gmail.com>
2024-11-07 13:48:20 +0200

    refactor: tweaks

--- test.rb
+++ test.rb
@@ -1,9 +1,10 @@
+# TODO
 require "minitest/autorun"
 
 class TestMeme < Minitest::Test
   def setup
     @meme = Meme.new
-  end
+  end 
 
   def test_that_kitty_can_eat
     assert_equal "OHAI!", @meme.i_can_has_cheezburger?
